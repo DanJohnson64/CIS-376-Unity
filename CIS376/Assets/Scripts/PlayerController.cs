dcdd3a76470973a13ad078799ff5f20a39b97606
DanJohnson64 <68051250+DanJohnson64@users.noreply.github.com>
2024-04-03 21:50:11 -0400

    Character Controller added

---
+++ CIS376/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+    Rigidbody2D rigidBody;
+
+    private bool IsMoving { get; set; }
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(moveInput.x * walkSpeed, rigidBody.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+    }
+}
